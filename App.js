0e7a415a9c7d68ca58c7aacf375c280e3d409b32
serkansentilay <sentiilay@gmail.com>
2025-08-03 17:16:40 +0300

    first app

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import React from "react"
+import { Text, StyleSheet, View } from 'react-native';
+
+function App() {
+
+  return (
+    <View style={styles.container}>
+      <Text>asdasd</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding:50
+  },
+});
+
+export default App;
